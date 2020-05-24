9d658abceee8edef46d50d4ecad11fd783859378
ravenmyst <mystique5022@gmail.com>
2020-05-24 18:14:00 -0400

    Minor optimize new regex and increase all speeds of parser

--- Classes/LogParser.cs
+++ Classes/LogParser.cs
@@ -34,7 +34,7 @@ namespace NWNLogRotator.Classes
                 }
                 eventLines.AddRange(eventLinesTemp);
             }
-            
+
             string text;
             if (removeEvents)
             {
@@ -54,12 +54,17 @@ namespace NWNLogRotator.Classes
                 text = text.Replace(exp, "");
 
             removeExps.AddRange(garbageLines);
+            Match HasCrafting = Regex.Match(text, @"\[(?:Applying|Removing) crafting effects\]\s*?\r\n");
+            if (HasCrafting.Length > 0) {
+                removeExps.AddRange(craftingLines);
+            }
             if (removeCombat) removeExps.AddRange(combatLines);
             foreach (var exp in removeExps)
                 text = exp.Replace(text, "");
 
+            
 
-            formatReplacesOrdered = formatReplacesWithUserOverride( CustomEmotes );
+            formatReplacesOrdered = formatReplacesWithUserOverride(CustomEmotes);
 
             foreach (var exp in formatReplacesOrdered)
                 text = exp.Item1.Replace(text, exp.Item2);
@@ -88,29 +93,29 @@ namespace NWNLogRotator.Classes
             string HTMLHeader = "<head>" +
                 "<style>" +
                     ".logbody { background-color: #000000; font-family: Tahoma, Geneva, sans-serif; color: #FFFFFF; }";
-                    HTMLHeader += ".logheader { color: #";
-                    if (ServerNameColor != "")
-                    {
-                        HTMLHeader += ServerNameColor;
-                    }
-                    HTMLHeader += " }" +
-                    ".default { color: #FFFFFF }" +
-                    ".timestamp { color: #B1A2BD; }" +
-                    ".actors { color: #8F7FFF; }" +
-                    ".tells { color: #0F0; }" +
-                    ".whispers { color: #808080; }" +
-                    ".emotes { color: #ffaed6; }" +
-                "</style>" +
-            "</head>";
+            HTMLHeader += ".logheader { color: #";
+            if (ServerNameColor != "")
+            {
+                HTMLHeader += ServerNameColor;
+            }
+            HTMLHeader += " }" +
+            ".default { color: #FFFFFF }" +
+            ".timestamp { color: #B1A2BD; }" +
+            ".actors { color: #8F7FFF; }" +
+            ".tells { color: #0F0; }" +
+            ".whispers { color: #808080; }" +
+            ".emotes { color: #ffaed6; }" +
+        "</style>" +
+    "</head>";
 
             string logTitle;
             if (ServerName != "")
             {
-                 logTitle = "<h4>[<span class='logheader'>" + ServerName + " Log</span>] ";
+                logTitle = "<h4>[<span class='logheader'>" + ServerName + " Log</span>] ";
             }
             else
             {
-                 logTitle = "<h4>[<span class='logheader'>Log</span>] ";
+                logTitle = "<h4>[<span class='logheader'>Log</span>] ";
             }
             logTitle += "<span class='actors'>Date/Time</span>: " + DateTime.Now.ToString("MM/dd/yyyy h:mm tt");
             logTitle += "</h4>";
@@ -118,7 +123,7 @@ namespace NWNLogRotator.Classes
             return "<html>" + HTMLHeader + logTitle + ParsedNWNLog + "<body class='logbody'><span class='default'>" + postLog;
         }
 
-        private List<Tuple<Regex, string>> formatReplacesWithUserOverride( string CustomEmotes )
+        private List<Tuple<Regex, string>> formatReplacesWithUserOverride(string CustomEmotes)
         {
             string[] emotesArray = CustomEmotes.Split(',');
             List<Tuple<Regex, string>> formatReplacesOrderedReturn = new List<Tuple<Regex, string>>();
@@ -133,8 +138,8 @@ namespace NWNLogRotator.Classes
                     string theEmotePair = emotePair.Trim();
                     if (theEmotePair.Length == 2)
                     {
-                        string tempLeftBracket = theEmotePair.Substring(0,1);
-                        string tempRightBracket = theEmotePair.Substring(1,1);
+                        string tempLeftBracket = theEmotePair.Substring(0, 1);
+                        string tempRightBracket = theEmotePair.Substring(1, 1);
                         string theRegEx;
                         theRegEx = "\\" + tempLeftBracket + "(?!([0-9]{2}\\:[0-9]{2}|Whisper|Tell)).*\\" + tempRightBracket;
 
@@ -153,7 +158,7 @@ namespace NWNLogRotator.Classes
                 }
                 formatReplacesOrderedReturn.AddRange(additionalEmotesList);
             }
-       
+
             formatReplacesOrderedReturn.AddRange(formatReplacesOrderedThree);
 
             return formatReplacesOrderedReturn;
@@ -226,7 +231,7 @@ namespace NWNLogRotator.Classes
             "Ignore the crafting roll and gold message for robes."
         };
 
-        private static string timestampMatch = @".+?(?=.*)"; 
+        private static string timestampMatch = @".+?(?=.*)";
         private static string timestampExactMatch = @"\[\w\w\w\s\w\w\w\s\d\d\s\d\d:\d\d:\d\d\]\s";
 
         private List<Regex> combatLines = new List<Regex>
@@ -270,35 +275,37 @@ namespace NWNLogRotator.Classes
         };
         private List<Regex> garbageLines = new List<Regex>
         {
-            new Regex( @"nwsync\:\s?storage\s?at\s?[0-9:A-z\,= ]{10,}\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( @"nwsync\:\s?migrations\s?currently\s?applied\:\s?\d{1,}\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( @"nwsync\:\s?shard\s?\d{1,}\s?available,\sspace\sused\:\s?\d{1,}\skb\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( @"game\s?is\s?using\s?local\s?port\:\s?\d{1,}\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( @"Error\:\s?\d{1,}\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( @"gog\:\s?authentication\s?failed\:\s?\d{1,}\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@"Script\s.*,\sOID\:.*,\sTag\:\s.*,\sERROR\:\sTOO MANY INSTRUCTIONS\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            // Crafting-related removing.
-            new Regex( timestampMatch+@"\[(?:Applying|Removing) crafting effects\]\s*?\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@".*?\:\sJump\s\d+\s(?:part|colou?r)s\s(?:forward|backward)\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@".*?\:\sSelect\sthe\scolou?r\stype\sthat\syou\swant\sto\schange\.\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@"Lost\sItem\:\s[A-z\s\(\)\d]+(?=\r\n)", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@"Colou?r\sset\sto\:\s[A-z\s\(\)\d]+\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@"Current Colou?r(\stype)?\:\s[A-z\s\(\)\d]+\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@"Current\spart\:\s\d+(\(\d+\))?\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@".*?\:\s(?:Next|Previous|Change)\sColou?r\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@".*?\:\s(?:Next|Previous|Change)\sColou?r\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@".*?\:\sChange\s(?:Right|Left)?\s?[A-z]+\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( timestampMatch+@".*?\:\s(Next|Previous) part\r\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase ),
-            new Regex( @"\r\n(?=\r\n)", RegexOptions.Compiled | RegexOptions.Multiline ),
+            new Regex( @"nwsync\:\s?Storage\s?at\s?[0-9:A-z\,= ]{10,}\r\n", RegexOptions.Compiled),
+            new Regex( @"nwsync\:\s?Migrations\s?currently\s?applied\:\s?\d{1,}\r\n", RegexOptions.Compiled),
+            new Regex( @"nwsync\:\s?Shard\s?\d{1,}\s?available,\sSpace\sUsed\:\s?\d{1,}\sKB\r\n", RegexOptions.Compiled),
+            new Regex( @"Game\s?is\s?using\s?local\s?port\:\s?\d{1,}\r\n", RegexOptions.Compiled),
+            new Regex( @"Error\:\s?\d{1,}\r\n", RegexOptions.Compiled),
+            new Regex( @"GOG\:\s?Authentication\s?failed\:\s?\d{1,}\r\n", RegexOptions.Compiled),
+            new Regex( timestampMatch+@"Script\s.*,\sOID\:.*,\sTag\:\s.*,\sERROR\:\sTOO MANY INSTRUCTIONS\r\n", RegexOptions.Compiled),
+            new Regex( @"\r\n(?=\r\n)", RegexOptions.Compiled),
+        };
+        private List<Regex> craftingLines = new List<Regex>
+        {
+            new Regex(timestampMatch + @"\[(?:Applying|Removing) crafting effects\]\s*?\r\n"),
+            new Regex(timestampMatch + @".*?\:\sJump\s\d+\s(?:part|colou?r)s\s(?:forward|backward)\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\sSelect\sthe\scolou?r\stype\sthat\syou\swant\sto\schange\.\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @"Lost\sItem\:\s[A-z\s\(\)\d]+(?=\r\n)", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @"Colou?r\sset\sto\:\s[A-z\s\(\)\d]+\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @"Current Colou?r(\stype)?\:\s[A-z\s\(\)\d]+\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @"Current\spart\:\s\d+(\(\d+\))?\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\s(?:Next|Previous|Change)\sColou?r\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\s(?:Next|Previous|Change)\sColou?r\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\sChange\s(?:Right|Left)?\s?[A-z]+\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\s(Next|Previous) part\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
             // Try compound statements, then individual lines if they still remain.
-            new Regex( timestampMatch+@".*?\:\sChange\s(?:Cloth|Metal|Leather)\s\d+\r\n"+timestampExactMatch+@".*?\:\sBack\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@".*?\:\sWhich\spart\sdo\syou\s\want\sto\schange\?\r\n("+timestampExactMatch+@".*?\:\s(?:Right|Left)\s?[A-z]*?\r\n)?"+timestampExactMatch+@".*?\:\sBack\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@".*?\:\sWhat\sdo\syou\swant\sto\smodify\?\r\n("+timestampExactMatch+@".*?\:\s(?:Armour|Weapon)\s?(?:Colours|Appearance)\r\n)?"+timestampExactMatch+@".*?\:\sBack\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
+            new Regex(timestampMatch + @".*?\:\sChange\s(?:Cloth|Metal|Leather)\s\d+\r\n" + timestampExactMatch + @".*?\:\sBack\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\sWhich\spart\sdo\syou\s\want\sto\schange\?\r\n(" + timestampExactMatch + @".*?\:\s(?:Right|Left)\s?[A-z]*?\r\n)?" + timestampExactMatch + @".*?\:\sBack\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\sWhat\sdo\syou\swant\sto\smodify\?\r\n(" + timestampExactMatch + @".*?\:\s(?:Armour|Weapon)\s?(?:Colours|Appearance)\r\n)?" + timestampExactMatch + @".*?\:\sBack\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
             // Fallback compounds
-            new Regex( timestampMatch+@".*?\:\sWhich\spart\sdo\syou\s\want\sto\schange\?\r\n"+timestampExactMatch+@".*?\:\s((?:Right|Left)\s?[A-z]*?|Neck|Back|Belt|Helmet|Armour)\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@".*?\:\sChange\s(?:Cloth|Metal|Leather)\s\d+\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( timestampMatch+@".*?\:\sWhat\sdo\syou\swant\sto\smodify\?\r\n"+timestampExactMatch+@".*?\:\s(?:Armour|Weapon)\s?(?:Colours|Appearance)\r\n", RegexOptions.Compiled | RegexOptions.Multiline ),
-            new Regex( @"\r\n(?=\r\n)", RegexOptions.Compiled | RegexOptions.Multiline ),
+            new Regex(timestampMatch + @".*?\:\sWhich\spart\sdo\syou\s\want\sto\schange\?\r\n" + timestampExactMatch + @".*?\:\s((?:Right|Left)\s?[A-z]*?|Neck|Back|Belt|Helmet|Armour)\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\sChange\s(?:Cloth|Metal|Leather)\s\d+\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(timestampMatch + @".*?\:\sWhat\sdo\syou\swant\sto\smodify\?\r\n" + timestampExactMatch + @".*?\:\s(?:Armour|Weapon)\s?(?:Colours|Appearance)\r\n", RegexOptions.Compiled | RegexOptions.Multiline),
+            new Regex(@"\r\n(?=\r\n)", RegexOptions.Compiled | RegexOptions.Multiline),
         };
     }
 }
